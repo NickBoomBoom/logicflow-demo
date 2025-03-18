1db7cee02b4e1ba07c68f5761797d51b5bdd6921
q.chen <q.chen.self@outlook.com>
2025-03-18 11:49:08 +0800

    ignore

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// @ts-ignore
 import JsonViewer from "vue3-json-viewer";
 import "vue3-json-viewer/dist/index.css";
 const app = createApp(App);
